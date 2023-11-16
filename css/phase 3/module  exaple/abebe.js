44d88cadc96a56f442c894f34be3253fe5076753
MINTAbizu <150599734+MINTAbizu@users.noreply.github.com>
2023-11-16 10:56:18 +0300

    created abebe

---
+++ css/phase 3/module  exaple/abebe.js
@@ -0,0 +1,20 @@
+var AAvar1=1;
+var BBvar1=2;
+
+
+function AAsomefunctio(){
+    var AAsecondvar2=7;
+    console.log(AAvar1)
+
+    
+}
+     function sum(a,b){
+        var c;
+       c=(a+b);
+     
+       console.log(c);
+     }
+     console.log(module);
+     module.exports.AA=AAvar1;
+     module.exports.sum=sumfunction;
+     module.exports.bb=sumfunction;
